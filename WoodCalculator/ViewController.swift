255be7a8151ec2933313b62c8b359127ad9f9082
松村敬太 <MatsumuraKeita@matsumuratakashifutoshi-no-imac.local>
2018-04-14 20:58:53 +0900

    TextFieldの入力 入力値を取得し、計算して表示 入力終了時にキーボード格納 を実装

--- WoodCalculator/ViewController.swift
+++ WoodCalculator/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController,UITextFieldDelegate,UIPickerViewDelegate,UIPickerViewDataSource {
+    
+    @IBOutlet weak var kindOfWood: UILabel!
+    
+    @IBOutlet weak var length: UITextField! //長さ
+    @IBOutlet weak var width: UITextField!  //幅
+    @IBOutlet weak var height: UITextField! //厚さ
+    @IBOutlet weak var weight: UILabel!
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -18,14 +26,42 @@ class ViewController: UIViewController,UITextFieldDelegate,UIPickerViewDelegate,
     }
     
 
-    @IBOutlet weak var kindOfWood: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        length.placeholder = "1800"
+        width.placeholder = "1000"
+        height.placeholder = "65"
+        length.clearButtonMode = .always
+        width.clearButtonMode = .always
+        height.clearButtonMode = .always
+        length.returnKeyType = .done
+        width.returnKeyType = .done
+        height.returnKeyType = .done
+        self.length.delegate = self
+        self.width.delegate = self
+        self.height.delegate = self
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
     @IBAction func calculate(_ sender: Any) {
+        var taiseki:Int
+        //長さ、幅、厚さを取得
+            //空欄はダメ
+        //体積を求める
+        taiseki = Int(length.text!)! * Int(width.text!)! * Int(height.text!)!
+
+        //重さに表示
+        weight.text = String(taiseki)
     }
     
     override func didReceiveMemoryWarning() {
