c074cc49c99edb404f499ae7fc20974e28783bfc
松村敬太 <MatsumuraKeita@matsumuratakashifutoshi-no-imac.local>
2018-04-14 19:33:16 +0900

    initial commit

--- WoodCalculator/ViewController.swift
+++ WoodCalculator/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var kindOfWood: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func calculate(_ sender: Any) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
