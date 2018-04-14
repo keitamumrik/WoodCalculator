d215d05e074fe9dc59ebefd620bdeefeecd1bea4
松村敬太 <MatsumuraKeita@matsumuratakashifutoshi-no-imac.local>
2018-04-14 19:46:44 +0900

    iPhoneを繋いで表示だけ確認した

--- WoodCalculator/ViewController.swift
+++ WoodCalculator/ViewController.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate,UIPickerViewDelegate,UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 1
+    }
+    
 
     @IBOutlet weak var kindOfWood: UILabel!
 
