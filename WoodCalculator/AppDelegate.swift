1308b983fea0b7f757edb9143255c256ef9de413
松村敬太 <MatsumuraKeita@matsumuratakashifutoshi-no-imac.local>
2018-04-17 02:02:38 +0900

    起動画面splash作成 アイコン作成 起動時に3秒スリープ処理追加

--- WoodCalculator/AppDelegate.swift
+++ WoodCalculator/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //splash表示時間
+        sleep(3)
         return true
     }
 
